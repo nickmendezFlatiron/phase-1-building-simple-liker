924a318566ffdab98eb41f2cd6faa9aaf7a59e0c
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-02-11 23:43:36 -0500

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeGlyph = [...document.querySelectorAll('span.like-glyph')]
 
+likeGlyph.forEach(heart =>{
+  heart.addEventListener('click' , () => {
+    console.log(heart.innerText)
 
+    // Makes empty heart red and full when status is successful
+    if(heart.innerText === EMPTY_HEART) {
+      mimicServerCall()
+      .then(() => {
+        heart.innerText = FULL_HEART;
+        heart.classList = 'activated-heart'
+      })
 
+      //makes eror message appear at failed status , disappears after 3 seconds
+      .catch(() => { 
+        let modal = document.querySelector('#modal')
+        modal.classList.remove('hidden')
+        setTimeout(timeOut, 3000)
+    })
+    } else {                                      // Changes full heart to empty
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+    }
+    
+  })
+})
+
+function timeOut(){
+  modal.classList = 'hidden'
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
